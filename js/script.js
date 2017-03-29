2e45c09ff41924ed069686622e8eb303287e2e25
Ehdim <ehdim1@gmail.com>
2017-03-29 12:16:20 +0500

    alma

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    var element=document.querySelector('#drag1')
+    // element.style.marginTop='10px';
+  
+}
